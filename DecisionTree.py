f894e7fa4979766df474196297e91e3366229065
julianfdmontes <111426018+julianfdmontes@users.noreply.github.com>
2023-12-29 12:43:16 +0100

    Add files via upload

--- DecisionTree.py
+++ DecisionTree.py
@@ -12,16 +12,21 @@ class Node():
         return self.value is not None
 
 class DecisionTree():
-    def __init__(self, min_samples_split=2,max_depth=100, n_features=None):
+    def __init__(self, min_samples_split=2,max_depth=8, n_features=None):
         self.min_samples_split=min_samples_split
         self.max_depth=max_depth
         self.n_features=n_features
         self.root=None
+        global cuentas
+        cuentas = 1
+        self.d = {}
+
     def fit(self, X, y):
         self.n_features = X.shape[1] if not self.n_features else min(X.shape[1],self.n_features)
         self.root = self._grow_tree(X, y)
 
     def _grow_tree(self, X, y, depth=0):
+        global cuentas
         n_samples, n_feats = X.shape
         n_labels = len(np.unique(y))
 
@@ -33,13 +38,18 @@ class DecisionTree():
         feat_idxs = np.random.choice(n_feats, self.n_features, replace=False)
 
         # find the best split
-        best_feature, best_thresh = self._best_split(X, y, feat_idxs)
-
+        best_feature, best_thresh, best_gain = self._best_split(X, y, feat_idxs)
+        
+        self.d[cuentas] = {'feature':best_feature, 'thr':best_thresh, 'gain':best_gain}
         # create child nodes
+        # Importante, vuelve a llamar la función, no había visto esto
         left_idxs, right_idxs = self._split(X[:, best_feature], best_thresh)
         left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth+1)
         right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth+1)
+
+        cuentas = cuentas + 0.5
         return Node(best_feature, best_thresh, left, right)
+
 
     def _best_split(self, X, y, feat_idxs):
         best_gain = -1
@@ -58,4 +68,65 @@ class DecisionTree():
                     split_idx = feat_idx
                     split_threshold = thr
 
-        return split_idx, split_threshold
+        return split_idx, split_threshold, best_gain
+
+    def _information_gain(self, y, X_column, threshold):
+        # parent entropy
+        parent_entropy = self._criteria(y)
+
+        # create children
+        left_idxs, right_idxs = self._split(X_column, threshold)
+
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
+            return 0
+        
+        # calculate the weighted avg. entropy of children
+        n = len(y)
+        n_l, n_r = len(left_idxs), len(right_idxs)
+        e_l, e_r = self._criteria(y[left_idxs]), self._criteria(y[right_idxs])
+        child_entropy = (n_l/n) * e_l + (n_r/n) * e_r
+
+        # calculate the IG
+        information_gain = parent_entropy - child_entropy
+        return information_gain
+
+    def _split(self, X_column, split_thresh):
+        left_idxs = np.argwhere(X_column <= split_thresh).flatten()
+        right_idxs = np.argwhere(X_column > split_thresh).flatten()
+        return left_idxs, right_idxs
+
+    def _criteria(self, y, criteria='entropy'):
+        if criteria == 'entropy':
+            return self._entropy(y)
+        if criteria == 'gini':
+            return self._gini(y)
+
+    def _entropy(self, y):
+        hist = np.bincount(y)
+        ps = hist / len(y)
+        return -np.sum([p * np.log(p) for p in ps if p>0])
+
+    def _gini(self, y):
+        hist = np.bincount(y)
+        ps = hist / len(y)
+        return -np.sum([p * p for p in ps])
+
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        
+        # print(y)
+        # print(counter)
+        value = counter.most_common(1)[0][0]
+        return value
+
+    def predict(self, X):
+        return np.array([self._traverse_tree(x, self.root) for x in X])
+
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value
+
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
+
